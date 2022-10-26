7e1f614ef91787b5f9bea9d95e2ad1f02279f5b1
Sergejs Tiselskis <slokas85@gmail.con>
2022-10-26 23:33:00 +0300

    Initial Commit

---
+++ FamilyChat/FamilyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyChatApp.swift
+//  FamilyChat
+//
+//  Created by John Doe on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FamilyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
